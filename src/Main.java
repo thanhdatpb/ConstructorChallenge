437aafb2357fa9a3a1cf77cea6b248f307f9a42a
Thanh Dat <tthanhdat.pb@gmail.com>
2024-03-04 15:52:38 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        Customer customer = new Customer("Thanh Dat", 1000, "tthanhdat.pb@gmail.com" );
+        System.out.println(customer.getName());
+        System.out.println(customer.getCreditLimit());
+        System.out.println(customer.getEmail() );
+
+        Customer secondCustomer = new Customer();
+        System.out.println(secondCustomer.getName());
+        System.out.println(secondCustomer.getCreditLimit());
+        System.out.println(secondCustomer.getEmail() );
+
+        Customer thirdCustomer = new Customer("Joe", "joe@email.com");
+        System.out.println(thirdCustomer.getName());
+        System.out.println(thirdCustomer.getCreditLimit());
+        System.out.println(thirdCustomer.getEmail() );
+    }
+}
